cc0871b4486986581bef3c08cee2ea2f9f38f4e8
Veljko Naumovic <veljko.naumovic@eponuda.com>
2022-09-04 22:10:31 +0200

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Download, Features, SectionWraper } from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWraper
+        title="Your own store of Nifty NFTs .Start Selling and Gtowing"
+        description="Buy, store, collect NFTS, Exchange and earn crypto. Join 25+ million people using ProNef Marketplace"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWraper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of Pro/nef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWraper
+        title="Deployment"
+        description="Pronef is built using Expo which runs natively on all users devices. You can easyly get your app into people hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWraper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one show the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner-02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-column text-center banner-04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by <span className="bold">JavaScript Mastery</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Download from "./Download";
+import Features from "./Features";
+import Button from "./Button";
+import SectionWraper from "./SectionWraper";
+
+export { Download, Features, SectionWraper };
